283c66447a33b1e6bc34f5d89fe720c14c0d7fe9
eslami <ab.eslami2001@gmail.com>
2024-06-28 19:06:03 +0330

    feat: create constructor for piece game

--- src/ChessPiece.cpp
+++ src/ChessPiece.cpp
@@ -4,11 +4,11 @@
 
 using namespace std;
 
-ChessPiece::ChessPiece(bool color, Coordinate coordinate)
-    : m_color(color),
-    m_coordinate(coordinate){
-
-}
+ChessPiece::ChessPiece(bool color, Coordinate coordinate, vector<Coordinate> jump = {})
+    : m_color(color)
+    , m_coordinate(coordinate)
+    , m_jump(jump)
+{}
 
 bool ChessPiece::getColor(){
     return m_color;
@@ -50,37 +50,37 @@ vector<Coordinate> Bishop::availableMoves(ChessBoard& chessBoard) const {
 
 
 King::King(bool color, Coordinate coordinate)
-    :ChessPiece(color, coordinate),
-    m_isMoved(false){
-
-}
+    :ChessPiece(color, coordinate, vector<Coordinate>{{0, 1}, {1, 1}, 
+        {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}})
+    ,m_isMoved(false)
+{}
 
 Queen::Queen(bool color, Coordinate coordinate)
-    :ChessPiece(color, coordinate){
-
-}
+    :ChessPiece(color, coordinate, vector<Coordinate>{{0, 1}, {1, 1}, 
+        {1, 0}, {1, -1}, {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}})
+{}
 
 Rook::Rook(bool color, Coordinate coordinate)
-    :ChessPiece(color, coordinate),
-    m_isMoved(false){
-
-}
+    :ChessPiece(color, coordinate, vector<Coordinate>{{0, 1}, {1, 0}, {0, -1}, {-1, 0}})
+    ,m_isMoved(false)
+{}
 
 Bishop::Bishop(bool color, Coordinate coordinate)
-    :ChessPiece(color, coordinate){
-
-}
+    :ChessPiece(color, coordinate, vector<Coordinate>{{1, 1}, {1, -1}, {-1, -1}, {-1, 1}})
+{}
 
 Knight::Knight(bool color, Coordinate coordinate)
-    :ChessPiece(color, coordinate){
-
-}
+    :ChessPiece(color, coordinate, {Coordinate{1, 2}, Coordinate{2, 1}, 
+        Coordinate{1, -2}, Coordinate{-2, 1}, Coordinate{-1, 2}, 
+            Coordinate{2, -1}, Coordinate{-1, -2}, Coordinate{-2, -1}})
+{}
 
 Pawn::Pawn(bool color, Coordinate coordinate)
-    :ChessPiece(color, coordinate),
-    m_isMoved(false){
-
-}
+    : ChessPiece(color, coordinate, color ? 
+        vector<Coordinate>{{0, 1}, {0, 2}, {1, 1}, {-1, 1}} :
+        vector<Coordinate>{{0, -1}, {0, -2}, {1, -1}, {-1, -1}})
+    , m_isMoved(false)
+{}
 
 char King::getChar(){
     return 'k';
--- src/ChessPiece.hpp
+++ src/ChessPiece.hpp
@@ -8,7 +8,7 @@ class ChessBoard;
 
 class ChessPiece {
 public:
-    ChessPiece(bool, Coordinate);
+    ChessPiece(bool, Coordinate, std::vector<Coordinate>);
     ~ChessPiece() = default;
     virtual std::vector<Coordinate>availableMoves(ChessBoard&) const;
     Coordinate getCoordinate();
@@ -17,7 +17,6 @@ public:
 
 protected:
     bool m_color;
-    bool m_isIn;
     Coordinate m_coordinate;
     const std::vector<Coordinate> m_jump;
 
