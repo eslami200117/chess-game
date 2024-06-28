b4db2cd25e779a0ee7129f6826dd673d5bc7b539
eslami <ab.eslami2001@gmail.com>
2024-06-28 16:58:17 +0330

    feat: add operator to coordinate

--- src/Coordinate.hpp
+++ src/Coordinate.hpp
@@ -6,19 +6,20 @@ struct Coordinate {
     
     Coordinate  (int x=0, int y=0) 
         : x(x), y(y)
-    {
-    }
+    {}
 
-    Coordinate& operator=(const Coordinate& a)
-    {
+    Coordinate& operator=(const Coordinate& a){
         x=a.x;
         y=a.y;
         return *this;
     }
 
-    Coordinate operator+(const Coordinate& a) const
-    {
+    Coordinate operator+(const Coordinate& a) const{
         return Coordinate(a.x+x, a.y+y);
+    }
+
+    bool operator==(const Coordinate& a) const {
+        return x == a.x && y == a.y;
     }
 };
 
